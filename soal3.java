5e8fcbf307bf85b2d14adb235f2cfaa041889da6
tmalikkamal <112326508+tmalikkamal@users.noreply.github.com>
2022-09-07 16:14:16 +0700

    Add files via upload

---
+++ soal3.java
@@ -0,0 +1,13 @@
+public class soal3 {
+    public static void main(String arg[])
+{
+for (int i=0; i<=5; i++)
+{
+for (int j=5; j>i; j--)
+{
+System.out.print('*');
+}
+System.out.println();
+        }
+    }
+}
