42cd899cc18cc275521761b69d77d09d438def8b
tmalikkamal <112326508+tmalikkamal@users.noreply.github.com>
2022-09-08 08:31:49 +0700

    Add files via upload

---
+++ Praktikum 2/soal1.java
@@ -0,0 +1,14 @@
+/** 
+* Program berikut untuk mencoba operator aritmetika
+*/
+public class soal1 {
+    public static void main(String args[]) {
+        int num1 = 3;
+        int num2 = 2;
+        System.out.println("num1 + num2 = " + (num1 + num2) );
+        System.out.println("num1 - num2 = " + (num1 - num2) );
+        System.out.println("num1 * num2 = " + (num1 * num2) );
+        System.out.println("num1 / num2 = " + ((double)num1 / num2) );
+        System.out.println("num1 % num2 = " + (num1 % num2) );
+        }
+}
